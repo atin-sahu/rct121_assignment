3de24b326c79852979e4b2677a52575f05344640
Atin Sahu <97465321+atin-sahu@users.noreply.github.com>
2022-08-03 17:46:15 +0530

    start practice

---
+++ selftPractice/todoRedux/src/components/Count.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Count = () => {
+
+
+
+  return (
+    <div>
+        <div>Count</div>
+        <button >increase</button>
+        <button>decrease</button>
+    </div>
+    
+  )
+}
